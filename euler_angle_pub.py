017d576dc88ea741e956b9ed03ff21b334305627
Incheol-Shine <soicie1243@naver.com>
2023-03-08 12:48:24 +0000

    ros2coppelia add

---
+++ euler_angle_pub.py
@@ -0,0 +1,66 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import	serial
+import	struct
+from	typing	import Final
+
+""" constant """
+DATA_START:	Final = 0xAA
+DATA_END:	Final = 0xDD
+DATA_SIZE:	Final = 16
+DEG_2_RAD:	Final =  0.01745329251994329576923690768489
+ALPHA:		Final = 0.5
+
+class SerialReceiver:
+	def __init__(self):
+		self.euler_x = 0
+		self.euler_y = 0
+		self.mean_x = 0
+		self.mean_y = 0
+		self.button = 0
+
+class SimplePublisher(Node):
+
+	def __init__(self):
+		super().__init__('euler_angle_pub')
+		self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+
+def read_data(ser, receiver):
+	if ser.readable():
+		if (ser.in_waiting >= DATA_SIZE):
+			data = ser.read()
+			if data[0] != DATA_START:
+				return
+			data = ser.read(DATA_SIZE - 1)
+			if data[-1] != DATA_END:
+				return
+			else:
+				receiver.euler_x = struct.unpack('f', data[0:4])[0]
+				receiver.euler_y = struct.unpack('f', data[4:8])[0]
+				receiver.mean_x = receiver.mean_x * ALPHA + receiver.euler_x * (1 - ALPHA)
+				receiver.mean_y = receiver.mean_y * ALPHA + receiver.euler_y * (1 - ALPHA)
+				receiver.is_move = data[12]
+				receiver.button = data[13]
+
+def main(args=None):
+	ser = serial.Serial("/dev/ttyUSB0", 115200)
+	receiver = SerialReceiver()
+	# initialize the ROS communication
+	rclpy.init(args=args)
+	# declare the node constructor
+	simple_publisher = SimplePublisher()
+	msg = Twist()
+	while rclpy.ok():
+		read_data(ser, receiver)
+		msg.angular.x = float(receiver.mean_x)
+		msg.angular.y = float(receiver.mean_y)
+		print(f"x:{msg.angular.x}, y:{msg.angular.y}")
+		simple_publisher.publisher_.publish(msg)
+	# Explicity destroys the node
+	simple_publisher.destroy_node()
+	# shutdown the ROS communication
+	rclpy.shutdown()
+
+if __name__ == '__main__':
+	main()
